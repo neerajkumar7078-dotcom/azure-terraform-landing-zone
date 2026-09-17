165f2a1fb1762d292711cee156c596924506fe3a
Neeraj Kumar <neerajkumar7078@gmail.com>
2026-09-18 01:53:04 +0530

    Delete modules/network directory

--- modules/network/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-dev"
-  location            = "Central India"
-  resource_group_name = "rg-dev"
-
-  address_space = ["10.0.0.0/16"]
-}
