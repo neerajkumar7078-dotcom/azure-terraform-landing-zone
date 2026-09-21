877149a22fe8c393125e178d9264a9bc4fa3fcdb
Neeraj Kumar <neerajkumar7078@gmail.com>
2026-09-22 01:15:26 +0530

    Change storage account name in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateprod001"
+    storage_account_name = "stlandingzonetf"
     container_name       = "tfstate"
     key                  = "landingzone.tfstate"
   }
