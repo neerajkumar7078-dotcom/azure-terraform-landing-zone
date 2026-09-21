362c07f89d049410cbea6841bfe58bcd8a897a13
Neeraj Kumar <neerajkumar7078@gmail.com>
2026-09-22 00:40:37 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateprod001"
-    container_name       = "tfstate"
-    key                  = "landingzone.tfstate"
-  }
-}
