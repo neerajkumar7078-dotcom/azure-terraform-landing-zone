e7e097ba6265cb3eb96857c48924150f959afca2
Neeraj Kumar <neerajkumar7078@gmail.com>
2026-09-22 01:08:32 +0530

    Create Resource Group VNet and Subnets
    Added resources for resource group, virtual network, and multiple subnets.

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
-module "network" {
-  source = "./modules/network"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "snet-public"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = "snet-app"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "snet-private"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.3.0/24"]
 }
